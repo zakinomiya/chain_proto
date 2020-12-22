44a49b87a56fa63e65ddd521a19547ec94272051
AtsushiMiyazaki <ki929.oa95cp@gmail.com>
2020-12-22 09:19:59 +0900

    add account functinalities

--- blockchain/block.go
+++ blockchain/block.go
@@ -1,19 +1,27 @@
 package blockchain
 
 import (
+	"go_chain/account"
 	"go_chain/block"
+	"go_chain/transaction"
 	"log"
 )
 
+// Blocks returns Blockchain.blocks
 func (bc *Blockchain) Blocks() []*block.Block {
 	return bc.blocks
 }
 
+// ReplaceBlocks replaces the entire chain with the new one.
 func (bc *Blockchain) ReplaceBlocks(blocks []*block.Block) {
 	bc.blocks = blocks
 }
 
+// AddBlock adds a new block to the chain.
 func (bc *Blockchain) AddBlock(block *block.Block) bool {
+	bc.mutex.Lock()
+	defer bc.mutex.Unlock()
+
 	if !block.Verify() {
 		log.Println("info: Refused adding the block")
 		return false
@@ -21,7 +29,7 @@ func (bc *Blockchain) AddBlock(block *block.Block) bool {
 
 	log.Printf("info: Adding new block: %x\n", block.Hash)
 	log.Printf("debug: block=%+v\n", block)
-	log.Printf("info: Now the length of the chain is %d:\n", len(bc.blocks))
+	log.Printf("info: Now the length of the chain is %d:\n", bc.LatestBlock().Height)
 
 	if err := bc.repository.Insert(block); err != nil {
 		log.Printf("error: failed to insert block to db. err=%v", err)
@@ -31,4 +39,32 @@ func (bc *Blockchain) AddBlock(block *block.Block) bool {
 	bc.blocks = append(bc.blocks, block)
 	bc.SendEvent(NewBlock)
 	return true
+}
+
+func (bc *Blockchain) processTxs(txs []*transaction.Transaction) (map[string]*account.Account, error) {
+	log.Println("debug: processTxs")
+	accounts := map[string]*account.Account{}
+
+	for _, tx := range txs {
+		sender := accounts[tx.SenderAddr]
+		if sender == nil {
+			sender = account.New(tx.SenderAddr)
+			accounts[tx.SenderAddr] = sender
+		}
+
+		for _, output := range tx.Outs {
+			recipient := accounts[output.RecipientAddr]
+			if recipient == nil {
+				recipient = account.New(output.RecipientAddr)
+				accounts[output.RecipientAddr] = recipient
+			}
+
+			if err := sender.Send(output.Value, recipient); err != nil {
+				log.Printf("error: failed to send amount from %s to %s\n", sender.Addr(), recipient.Addr())
+				return nil, err
+			}
+		}
+	}
+
+	return accounts, nil
 }
--- blockchain/chain.go
+++ blockchain/chain.go
@@ -9,14 +9,18 @@ import (
 	"sync"
 )
 
+// BlockchainEvents represents varieties of events the blockchain invokes
+// Subscribers should register a channel which events will be streamed into.
 type BlockchainEvents string
 
 const (
+	// NewBlock event is invokes when a new block is added to the chain
 	NewBlock BlockchainEvents = "NEW_BLOCK"
 )
 
+// Blockchain is a struct of the chain
 type Blockchain struct {
-	lock          sync.RWMutex
+	mutex         sync.Mutex
 	chainID       uint32
 	subscriptions map[string]chan BlockchainEvents
 	height        uint32
@@ -27,6 +31,7 @@ type Blockchain struct {
 var blockchain *Blockchain
 var once sync.Once
 
+// New returns a new blockchain
 func New(chainID uint32, repository *repository.Repository) *Blockchain {
 	blockchain = &Blockchain{
 		chainID:       chainID,
@@ -63,10 +68,13 @@ func initializeBlockchain() error {
 	return nil
 }
 
+// ServiceName returns its service name
 func (bc *Blockchain) ServiceName() string {
 	return "Blockchain"
 }
 
+// Start starts intialises and starts the blockchain
+// There must be only one blockchain during the runtime.
 func (bc *Blockchain) Start() error {
 	once.Do(func() {
 		if err := initializeBlockchain(); err != nil {
@@ -78,32 +86,43 @@ func (bc *Blockchain) Start() error {
 	return nil
 }
 
+// Stop stops the blockchain gracefully.
+// TODO inplement Stop
 func (bc *Blockchain) Stop() {
 	return
 }
 
+// CurrentBlockHeight returns the height of the latest block in the chain.
+// The value does not necessarilly match the length of Blockchain.blocks.
 func (bc *Blockchain) CurrentBlockHeight() uint32 {
 	return bc.LatestBlock().Height
 }
 
+// LatestBlock returns the block at the last index of the Blockchain.blocks.
 func (bc *Blockchain) LatestBlock() *block.Block {
 	return bc.blocks[len(bc.blocks)-1]
 }
 
+// Difficulty returns the next mining target.
+// TODO implement Difficulty
 func (bc *Blockchain) Difficulty() uint32 {
 	return 5
 }
 
+// Subscribe regeisters a new channel to the subscription.
+// Blockchain events are streaming through the channel
 func (bc *Blockchain) Subscribe(key string) <-chan BlockchainEvents {
 	ch := make(chan BlockchainEvents)
 	bc.subscriptions[key] = ch
 	return ch
 }
 
+// Unsubscribe delets a subscription with the specified key.
 func (bc *Blockchain) Unsubscribe(key string) {
 	delete(bc.subscriptions, key)
 }
 
+// SendEvent sends events to the subscribed channels.
 func (bc *Blockchain) SendEvent(eventName BlockchainEvents) {
 	for key, ch := range bc.subscriptions {
 		log.Printf("debug: sending event(%s) to the subsctiption(%s)\n", eventName, key)
